2f47c0945b32bde4e571cc5bc575386e855f3e84
DESKTOP-BKI77CE\kashi <huzaifakashif202@gmail.com>
2025-02-26 02:34:11 +0500

    added all starter files and some js

---
+++ script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+let score0 = document.getElementById('score--0');
+let score1 = document.getElementById('score--1');
+let dice = document.querySelector('.dice');
+
+score0.textContent = 0;
+score1.textContent = 0;
+dice.classList.add('hidden');
